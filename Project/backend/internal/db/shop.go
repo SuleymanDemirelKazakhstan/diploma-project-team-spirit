07b09de195a9091ae0c2ff3f755092bacf12d616
4Aray2 <55088540+4Aray2@users.noreply.github.com>
2022-06-17 04:46:46 +0600

    Update shop.go

--- Project/backend/internal/db/shop.go
+++ Project/backend/internal/db/shop.go
@@ -579,42 +579,22 @@ func (o *OwnerRepo) MainPage(id *models.IdReg) (*models.MainPage, []models.Owner
 	}
 
 	var param models.MainPage
-	var cnull sql.NullInt64
 	sqlStatement = `SELECT DISTINCT count(product_id) FROM orders where shop_id=$1;`
 	row := o.db.QueryRow(sqlStatement, id.Id)
-	if err := row.Scan(&cnull); err != nil {
+	if err := row.Scan(&param.Customers); err != nil {
 		return &models.MainPage{}, []models.OwnerProduct{}, err
-	}
-	if cnull.Valid{
-		param.Customers = int(cnull.Int64)
-	}
-	if !cnull.Valid{
-		param.Customers = 0
 	}
 
 	sqlStatement = `SELECT count(order_id) FROM orders where shop_id=$1;`
 	row = o.db.QueryRow(sqlStatement, id.Id)
-	if err := row.Scan(&cnull); err != nil {
+	if err := row.Scan(&param.Orders); err != nil {
 		return &models.MainPage{}, []models.OwnerProduct{}, err
-	}
-	if cnull.Valid{
-		param.Orders = int(cnull.Int64)
-	}
-	if !cnull.Valid{
-		param.Orders = 0
 	}
 
 	sqlStatement = `SELECT sum(price-discount) FROM product where shop_id=$1;`
 	row = o.db.QueryRow(sqlStatement, id.Id)
-	if err := row.Scan(&cnull); err != nil {
+	if err := row.Scan(&param.Earnings); err != nil {
 		return &models.MainPage{}, []models.OwnerProduct{}, fmt.Errorf("database Earnings %w", err)
-	}
-	if cnull.Valid{
-		str12:= strconv.Itoa(int(cnull.Int64))
-		param.Earnings = str12
-	}
-	if !cnull.Valid{
-		param.Earnings = "0"
 	}
 
 	sqlStatement = `SELECT name FROM shop where shop_id=$1;`
@@ -625,14 +605,8 @@ func (o *OwnerRepo) MainPage(id *models.IdReg) (*models.MainPage, []models.Owner
 
 	sqlStatement = `SELECT count(*) FROM product where shop_id=$1 and selled_at is null;`
 	row = o.db.QueryRow(sqlStatement, id.Id)
-	if err := row.Scan(&cnull); err != nil {
+	if err := row.Scan(&param.Products); err != nil {
 		return &models.MainPage{}, []models.OwnerProduct{}, err
-	}
-	if cnull.Valid{
-		param.Products = int(cnull.Int64)
-	}
-	if !cnull.Valid{
-		param.Products = 0
 	}
 
 	return &param, products, nil
